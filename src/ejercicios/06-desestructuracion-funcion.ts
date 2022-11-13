1569919bcd84f9e0c82ba8636575e39500b4296d
Bryan <bryanstevenal@gmail.com>
2022-11-13 17:14:58 +0100

    Desestructuracion de funciones

---
+++ src/ejercicios/06-desestructuracion-funcion.ts
@@ -0,0 +1,47 @@
+
+
+console.log('Hola Mundo!!!');
+
+/*
+    ===== Código de TypeScript =====
+
+    DESESTRUCTURACIÓN DE ARGUMENTOS EN FUNCIONES
+    ============================================
+*/
+
+interface Producto {
+    desc : string;
+    precio : number;
+}
+
+const telefono : Producto = {
+    desc : 'Nokia A1',
+    precio : 150
+}
+
+const tableta : Producto = {
+    desc : 'iPad Air',
+    precio : 350
+}
+
+function calculaImpuestosSobreVenta ( productos : Producto[] ) : [number, number] {
+
+    let total = 0;
+
+    // productos.forEach( (producto, index, productosArr) => {
+    // Podemos seleccionar solo el precio del producto:
+    productos.forEach( ({ precio }, index, productosArr) => {
+        total += precio;
+    } );
+
+    return [total, total * 0.15];
+}
+
+const articulos = [ telefono, tableta ];
+
+const isv = calculaImpuestosSobreVenta( articulos );
+const [ total, impuestoSobreVentas ] = calculaImpuestosSobreVenta( articulos );
+
+console.log( 'ISV: ', isv );
+console.log( 'Total: ', total );
+console.log( 'Impuesto Sobre Ventas: ', impuestoSobreVentas );
